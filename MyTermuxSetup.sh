4fb1c4bb8d6611fe7d1f3d62ccfa804698ffc21d
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-16 10:27:14 +0100

    Add files via upload

---
+++ MyTermuxSetup.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+# Version 1.2
+# coded By Riccardo Papa
+# copyright(please read the license before sharing the code)
+clear
+echo """
+		 ____________________________________________
+		|			                     |
+		|        The Code By :- Riccardo Papa        |
+		|	 Telegram :- @PaposinDT              |
+		|        Facebook :- https://cutt.us/kfNBv   |
+		|        Youtube :-  https://cutt.us/fp49D   |
+		|        Git Hub :- github.com/PaposinDT     |
+                |        Instagram:- https://cutt.us/2ZDRu   |
+		|___________________________________________ | """
+sleep 3
+
+echo "                           	Version 1.1" 
+echo "                    	  Coded By Riccardo Papa"
+echo "            	  Want to support me?  paypal.me/ricky2006 "
+sleep 1
+echo "                            ENJOY YOUR TOOLS"
+sleep 1
+echo " Installing..."
+sleep 1
+mkdir Tools
+cd Tools
+mkdir Information-Gathering
+cd
+mkdir Hacking
+cd Hacking
+mkdir DDOS
+mkdir Phishing
+cd
+#
+#
+echo "INSTALLING GENERIC TOOLS"
+sleep 1
+cd Tools
+apt update
+apt install git
+echo "Downloading Tool-x"
+sleep 1
+git clone https://github.com/rajkumardusad/Tool-X.git
+cd Tool-X
+chmod +x install
+cd ..
+pkg install git -y 
+pkg install python -y 
+echo "Downloading TBomb"
+sleep 1
+git clone https://github.com/TheSpeedX/TBomb.git
+echo "Downloading unfollow-plus"
+sleep 1
+git clone git://github.com/htr-tech/unfollow-plus.git
+echo "Downloading ni_bomber"
+sleep 1
+git clone https://github.com/Nikait/ni_bomber
+echo "Downloading bash2mp4"
+sleep 1
+git clone https://github.com/htr-tech/bash2mp4
+#
+#
+#
+echo "INSTALLING UNFORMATION-GATHERING TOOLS"
+sleep 1
+echo "Downloading IP-Tracer"
+sleep 1
+git clone https://github.com/rajkumardusad/IP-Tracer.git
+cd IP-Tracer
+chmod +x install
+cd ..
+echo "Downloading osi.ig"
+sleep 1
+git clone https://github.com/th3unkn0n/osi.ig.git
+echo "Downloading TeleGram-Scraper"
+sleep 1
+apt install git curl -y
+git clone https://github.com/th3unkn0n/TeleGram-Scraper.git
+pip install lolcat
+echo "Downloading ipdrone"
+sleep 1
+pip install requests
+git clone https://github.com/noob-hackers/ipdrone
+cd
+#
+#
+#
+cd Hacking
+echo "INSTALLING HACKING TOOL"
+sleep 1
+pkg install git
+pkg install python
+echo "Downloading hacktronian"
+sleep 1
+git clone https://github.com/thehackingsage/hacktronian.git
+cd hacktronian
+chmod +x hacktronian.py
+cd ..
+pkg update
+pkg upgrade
+echo "Downloading ighack"
+sleep 1
+git clone https://github.com/noob-hackers/ighack
+echo "Downloading EasY_HaCk"
+sleep 1
+pkg install git
+git clone https://github.com/sabri-zaki/EasY_HaCk
+cd EasY_HaCk/
+chmod +x install.sh
+cd ..
+pkg update
+echo "Downloading onex"
+sleep 1
+pkg install python3 python3-pip git -y
+git clone https://github.com/rajkumardusad/onex.git
+cd onex
+chmod +x onex
+cd ..
+echo "Downloading hackerpro"
+sleep 1
+git clone https://github.com/jaykali/hackerpro.git
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+echo "Downloading infect"
+sleep 1
+git clone https://github.com/noob-hackers/infect
+echo "Downloading SocialBox-Termux"
+sleep 1
+git clone https://github.com/samsesh/SocialBox-Termux.git 
+cd SocialBox-Termux
+chmod +x SocialBox.sh
+chmod +x install-sb.sh
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+echo "Downloading grabcam"
+sleep 1
+git clone https://github.com/noob-hackers/grabcam
+echo "Downloading igbrute"
+sleep 1
+git clone https://github.com/dark-player/igbrute
+#
+#
+#
+echo "INSTALLING DDOS TOOLS"
+cd DDOS
+sleep 1
+echo "Downloading Impulse"
+sleep 1
+git clone https://github.com/LimerBoy/Impulse
+echo "Downloading XERXES"
+sleep 1
+git clone https://github.com/XCHADXFAQ77X/XERXES.git
+cd XERXES
+gcc -o xerxes xerxes.c
+cd ..
+echo "Downloading Vaim-sms"
+sleep 1
+git clone https://github.com/VaimpierOfficial/Vaim-sms
+echo "Downloading Ip-Attack"
+sleep 1
+git clone https://github.com/Bhai4You/Ip-Attack
+cd Ip-Attack
+chmod +x requirement.sh ip-attack.py
+cd ..
+cd ..
+#
+#
+#
+echo "INSTALLING PHISHING TOOLS"
+cd Phishing
+echo "Downloading zphisher"
+sleep 1
+git clone git://github.com/htr-tech/zphisher.git
+apt update
+apt install git -y
+echo "Downloading nexphisher"
+sleep 1
+git clone git://github.com/htr-tech/nexphisher.git
+echo "Downloading maskphish"
+sleep 1
+git clone https://github.com/jaykali/maskphish
+echo "Downloading mrphish"
+sleep 1
+git clone https://github.com/noob-hackers/mrphish
