a24fa263c30b453772494005af5aaa42a043041f
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-16 10:33:51 +0100

    Update TermuxSetup.sh

--- TermuxSetup.sh
+++ TermuxSetup.sh
@@ -18,11 +18,8 @@ sleep 3
 echo "                           	Version 1.1" 
 echo "                    	  Coded By Riccardo Papa"
 echo "            	  Want to support me?  paypal.me/ricky2006 "
-sleep 1
 echo "                            ENJOY YOUR TOOLS"
-sleep 1
 echo " Installing..."
-sleep 1
 mkdir Tools
 cd Tools
 mkdir Information-Gathering
@@ -35,12 +32,10 @@ cd
 #
 #
 echo "INSTALLING GENERIC TOOLS"
-sleep 1
 cd Tools
 apt update
 apt install git
 echo "Downloading Tool-x"
-sleep 1
 git clone https://github.com/rajkumardusad/Tool-X.git
 cd Tool-X
 chmod +x install
@@ -48,38 +43,29 @@ cd ..
 pkg install git -y 
 pkg install python -y 
 echo "Downloading TBomb"
-sleep 1
 git clone https://github.com/TheSpeedX/TBomb.git
 echo "Downloading unfollow-plus"
-sleep 1
 git clone git://github.com/htr-tech/unfollow-plus.git
 echo "Downloading ni_bomber"
-sleep 1
 git clone https://github.com/Nikait/ni_bomber
 echo "Downloading bash2mp4"
-sleep 1
 git clone https://github.com/htr-tech/bash2mp4
 #
 #
 #
 echo "INSTALLING UNFORMATION-GATHERING TOOLS"
-sleep 1
 echo "Downloading IP-Tracer"
-sleep 1
 git clone https://github.com/rajkumardusad/IP-Tracer.git
 cd IP-Tracer
 chmod +x install
 cd ..
 echo "Downloading osi.ig"
-sleep 1
 git clone https://github.com/th3unkn0n/osi.ig.git
 echo "Downloading TeleGram-Scraper"
-sleep 1
 apt install git curl -y
 git clone https://github.com/th3unkn0n/TeleGram-Scraper.git
 pip install lolcat
 echo "Downloading ipdrone"
-sleep 1
 pip install requests
 git clone https://github.com/noob-hackers/ipdrone
 cd
@@ -88,11 +74,9 @@ cd
 #
 cd Hacking
 echo "INSTALLING HACKING TOOL"
-sleep 1
 pkg install git
 pkg install python
 echo "Downloading hacktronian"
-sleep 1
 git clone https://github.com/thehackingsage/hacktronian.git
 cd hacktronian
 chmod +x hacktronian.py
@@ -100,10 +84,8 @@ cd ..
 pkg update
 pkg upgrade
 echo "Downloading ighack"
-sleep 1
 git clone https://github.com/noob-hackers/ighack
 echo "Downloading EasY_HaCk"
-sleep 1
 pkg install git
 git clone https://github.com/sabri-zaki/EasY_HaCk
 cd EasY_HaCk/
@@ -111,14 +93,12 @@ chmod +x install.sh
 cd ..
 pkg update
 echo "Downloading onex"
-sleep 1
 pkg install python3 python3-pip git -y
 git clone https://github.com/rajkumardusad/onex.git
 cd onex
 chmod +x onex
 cd ..
 echo "Downloading hackerpro"
-sleep 1
 git clone https://github.com/jaykali/hackerpro.git
 apt-get update -y
 apt-get upgrade -y
@@ -127,10 +107,8 @@ pkg install python2 -y
 pkg install git -y
 pip install lolcat
 echo "Downloading infect"
-sleep 1
 git clone https://github.com/noob-hackers/infect
 echo "Downloading SocialBox-Termux"
-sleep 1
 git clone https://github.com/samsesh/SocialBox-Termux.git 
 cd SocialBox-Termux
 chmod +x SocialBox.sh
@@ -142,31 +120,24 @@ pkg install python2 -y
 pkg install git -y
 pip install lolcat
 echo "Downloading grabcam"
-sleep 1
 git clone https://github.com/noob-hackers/grabcam
 echo "Downloading igbrute"
-sleep 1
 git clone https://github.com/dark-player/igbrute
 #
 #
 #
 echo "INSTALLING DDOS TOOLS"
 cd DDOS
-sleep 1
 echo "Downloading Impulse"
-sleep 1
 git clone https://github.com/LimerBoy/Impulse
 echo "Downloading XERXES"
-sleep 1
 git clone https://github.com/XCHADXFAQ77X/XERXES.git
 cd XERXES
 gcc -o xerxes xerxes.c
 cd ..
 echo "Downloading Vaim-sms"
-sleep 1
 git clone https://github.com/VaimpierOfficial/Vaim-sms
 echo "Downloading Ip-Attack"
-sleep 1
 git clone https://github.com/Bhai4You/Ip-Attack
 cd Ip-Attack
 chmod +x requirement.sh ip-attack.py
@@ -178,16 +149,12 @@ cd ..
 echo "INSTALLING PHISHING TOOLS"
 cd Phishing
 echo "Downloading zphisher"
-sleep 1
 git clone git://github.com/htr-tech/zphisher.git
 apt update
 apt install git -y
 echo "Downloading nexphisher"
-sleep 1
 git clone git://github.com/htr-tech/nexphisher.git
 echo "Downloading maskphish"
-sleep 1
 git clone https://github.com/jaykali/maskphish
 echo "Downloading mrphish"
-sleep 1
 git clone https://github.com/noob-hackers/mrphish
