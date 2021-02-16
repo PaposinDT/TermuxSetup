99de1717c7554f88aa2dc70af3f803eaa767a34d
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-16 15:22:30 +0100

    Update TermuxSetup.sh

--- TermuxSetup.sh
+++ TermuxSetup.sh
@@ -18,139 +18,113 @@ sleep 3
 echo "                           	Version 1.1" 
 echo "                    	  Coded By Riccardo Papa"
 echo "            	  Want to support me?  paypal.me/ricky2006 "
-echo "                            ENJOY YOUR TOOLS"
+sleep 1
+echo "			This tool is created for educational purpose only!
+ 				We use it to test under NDA agreements with clients and their consents
+  				and we never encourage to misuse or take responsibility for any damage caused !"
+sleep 6
 echo " Installing..."
 mkdir Tools
 cd Tools
 mkdir Information-Gathering
-cd ..
-mkdir Hacking
-cd Hacking
-mkdir DDOS
-mkdir Phishing
-cd ..
-#
-#
 echo "INSTALLING GENERIC TOOLS"
 cd Tools
 apt update
-echo "Downloading Tool-x"
-git clone https://github.com/rajkumardusad/Tool-X.git
-cd Tool-X
-chmod +x install
-cd ..
-pkg install git -y 
+# Downloading Tool-x
+git clone https://github.com/rajkumardusad/Tool-X.git 
 pkg install python -y 
-echo "Downloading TBomb"
+# Downloading TBomb
 git clone https://github.com/TheSpeedX/TBomb.git
-echo "Downloading unfollow-plus"
+# Downloading unfollow-plus
 git clone git://github.com/htr-tech/unfollow-plus.git
-echo "Downloading ni_bomber"
+# Downloading ni_bomber
 git clone https://github.com/Nikait/ni_bomber
-echo "Downloading bash2mp4"
+# Downloading bash2mp4
 git clone https://github.com/htr-tech/bash2mp4
+cd Information-Gathering
 #
 #
 #
-echo "INSTALLING UNFORMATION-GATHERING TOOLS"
-echo "Downloading IP-Tracer"
+echo "INSTALLING INFORMATION-GATHERING TOOLS"
+# Downloading IP-Tracer
 git clone https://github.com/rajkumardusad/IP-Tracer.git
-cd IP-Tracer
-chmod +x install
-cd ..
-echo "Downloading osi.ig"
+# Downloading osi.ig
 git clone https://github.com/th3unkn0n/osi.ig.git
-echo "Downloading TeleGram-Scraper"
-apt install curl -y
+# Downloading TeleGram-Scraper
+apt install git curl -y
 git clone https://github.com/th3unkn0n/TeleGram-Scraper.git
 pip install lolcat
-echo "Downloading ipdrone"
+# Downloading ipdrone
 pip install requests
 git clone https://github.com/noob-hackers/ipdrone
-cd
+cd ~
 #
 #
 #
+mkdir Hacking
 cd Hacking
 echo "INSTALLING HACKING TOOL"
+pkg install git
 pkg install python
-echo "Downloading hacktronian"
+# Downloading hacktronian
 git clone https://github.com/thehackingsage/hacktronian.git
-cd hacktronian
-chmod +x hacktronian.py
-cd ..
-pkg update
-pkg upgrade
-echo "Downloading ighack"
+# Downloading ighack
 git clone https://github.com/noob-hackers/ighack
-echo "Downloading EasY_HaCk"
+# Downloading EasY_HaCk
 pkg install git
 git clone https://github.com/sabri-zaki/EasY_HaCk
-cd EasY_HaCk/
-chmod +x install.sh
-cd ..
 pkg update
-echo "Downloading onex"
-pkg install python3 python3-pip -y
+# Downloading onex
+pkg install python3 python3-pip git -y
 git clone https://github.com/rajkumardusad/onex.git
-cd onex
-chmod +x onex
-cd ..
-echo "Downloading hackerpro"
+# Downloading hackerpro
 git clone https://github.com/jaykali/hackerpro.git
-apt-get update -y
-apt-get upgrade -y
-pkg install python -y
-pkg install python2 -y
-pip install lolcat
-echo "Downloading infect"
-git clone https://github.com/noob-hackers/infect
-echo "Downloading SocialBox-Termux"
-git clone https://github.com/samsesh/SocialBox-Termux.git 
-cd SocialBox-Termux
-chmod +x SocialBox.sh
-chmod +x install-sb.sh
 apt-get update -y
 apt-get upgrade -y
 pkg install python -y
 pkg install python2 -y
 pkg install git -y
 pip install lolcat
-echo "Downloading grabcam"
+# Downloading infect
+git clone https://github.com/noob-hackers/infect
+# Downloading SocialBox-Termux
+git clone https://github.com/samsesh/SocialBox-Termux.git 
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+# Downloading grabcam
 git clone https://github.com/noob-hackers/grabcam
-echo "Downloading igbrute"
+# Downloading igbrute
 git clone https://github.com/dark-player/igbrute
 #
 #
 #
+mkdir DDOS
+cd DDOS
 echo "INSTALLING DDOS TOOLS"
 cd DDOS
-echo "Downloading Impulse"
+# Downloading Impulse
 git clone https://github.com/LimerBoy/Impulse
-echo "Downloading XERXES"
+# Downloading XERXES
 git clone https://github.com/XCHADXFAQ77X/XERXES.git
-cd XERXES
-gcc -o xerxes xerxes.c
-cd ..
-echo "Downloading Vaim-sms"
+# Downloading Vaim-sms
 git clone https://github.com/VaimpierOfficial/Vaim-sms
-echo "Downloading Ip-Attack"
+# Downloading Ip-Attack
 git clone https://github.com/Bhai4You/Ip-Attack
-cd Ip-Attack
-chmod +x requirement.sh ip-attack.py
-cd ..
-cd ..
 #
 #
 #
+mkdir Phishing
 echo "INSTALLING PHISHING TOOLS"
 cd Phishing
-echo "Downloading zphisher"
+# Downloading zphisher
 git clone git://github.com/htr-tech/zphisher.git
 apt update
-echo "Downloading nexphisher"
+# Downloading nexphisher
 git clone git://github.com/htr-tech/nexphisher.git
-echo "Downloading maskphish"
+# Downloading maskphish
 git clone https://github.com/jaykali/maskphish
-echo "Downloading mrphish"
+# Downloading mrphish
 git clone https://github.com/noob-hackers/mrphish
+echo "INSTALLATION COMPLETED ENJOY YOUR HACKING TOOLS"
