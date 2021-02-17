5e80f8b6e95b86bab7eed875289fecbc68e6abe1
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-17 13:03:12 +0100

    Update Information-Gathering.sh

--- Information-Gathering.sh
+++ Information-Gathering.sh
@@ -1,6 +1,6 @@
 echo "INSTALLING INFORMATION-GATHERING TOOLS"
 mkdir Information-Gathering
-sudo bash DDOS.sh
+bash DDOS.sh
 cd Information-Gathering
 git clone https://github.com/rajkumardusad/IP-Tracer.git
 git clone https://github.com/th3unkn0n/osi.ig.git
