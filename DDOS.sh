bbd4ee8247bba970db7c9020d59e75446e100b07
Riccardo Papa <rickypapa2006@gmail.com>
2021-04-02 11:05:18 +0200

    Delete DDOS.sh

--- DDOS.sh
+++
@@ -1,8 +0,0 @@
-echo "INSTALLING DDOS TOOLS"
-mkdir DDOS
-cd DDOS
-git clone https://github.com/LimerBoy/Impulse
-git clone https://github.com/XCHADXFAQ77X/XERXES.git
-git clone https://github.com/VaimpierOfficial/Vaim-sms
-git clone https://github.com/Bhai4You/Ip-Attack
-echo "INSTALLATION COMPLETED ENJOY YOUR HACKING TOOLS"
