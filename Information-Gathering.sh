5752fa993a88ccb7afed338d51a8c24c13c644e2
Riccardo Papa <rickypapa2006@gmail.com>
2021-04-02 11:05:33 +0200

    Delete Information-Gathering.sh

--- Information-Gathering.sh
+++
@@ -1,8 +0,0 @@
-echo "INSTALLING INFORMATION-GATHERING TOOLS"
-mkdir Information-Gathering
-bash DDOS.sh
-cd Information-Gathering
-git clone https://github.com/rajkumardusad/IP-Tracer.git
-git clone https://github.com/th3unkn0n/osi.ig.git
-git clone https://github.com/th3unkn0n/TeleGram-Scraper.git
-git clone https://github.com/noob-hackers/ipdrone
