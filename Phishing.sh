988c576760708e517cc3410ed1a83dd1e345e015
Riccardo Papa <rickypapa2006@gmail.com>
2021-04-02 11:05:40 +0200

    Delete Phishing.sh

--- Phishing.sh
+++
@@ -1,8 +0,0 @@
-echo "INSTALLING PHISHING TOOLS"
-mkdir Phishing
-bash Information-Gathering.sh
-cd Phishing
-git clone git://github.com/htr-tech/zphisher.git
-git clone git://github.com/htr-tech/nexphisher.git
-git clone https://github.com/jaykali/maskphish
-git clone https://github.com/noob-hackers/mrphish
