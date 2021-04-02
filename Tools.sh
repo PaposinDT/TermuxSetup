b506c6a8f551fd2fbbd5602e0b145ae9ce9950a3
Riccardo Papa <rickypapa2006@gmail.com>
2021-04-02 11:05:52 +0200

    Delete Tools.sh

--- Tools.sh
+++
@@ -1,9 +0,0 @@
-echo "INSTALLING INFORMATION-GATHERING TOOLS"
-mkdir Tools
-bash Hacking.sh
-cd Tools
-git clone https://github.com/rajkumardusad/Tool-X.git
-git clone https://github.com/TheSpeedX/TBomb.git
-git clone git://github.com/htr-tech/unfollow-plus.git
-git clone https://github.com/Nikait/ni_bomber
-git clone https://github.com/htr-tech/bash2mp4
