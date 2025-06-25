7156755cfe15595a0cb559e02ce6f54122fbf08b
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 10:30:29 +0200

    Update Hacking.sh

--- Hacking.sh
+++ Hacking.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
-#Author.......: Riccardo Papa
-mkdir ./Hacking
+# Author.......: Riccardo Papa
+
+mkdir -p ./Hacking
 ./Phishing.sh
 cd ./Hacking
+
 echo
 echo "INSTALLING HACKING TOOLS"
 sleep 2
 echo "Cloning 10 repositories"
 echo
 sleep 2
-echo "Cloning hacktronian:              |-|-|-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/thehackingsage/hacktronian.git --quiet
-echo "Cloning ighack:                   |-|-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/noob-hackers/ighack --quiet
-echo "Cloning onex:                     |-|-|-|-|-|-|-|-|"
-git clone https://github.com/rajkumardusad/onex.git --quiet
-echo "Cloning hackerpro:                |-|-|-|-|-|-|-|"
-git clone https://github.com/jaykali/hackerpro.git --quiet
-echo "Cloning infect:                   |-|-|-|-|-|-|"
-git clone https://github.com/noob-hackers/infect --quiet
-echo "Cloning SocialBox-Termux:         |-|-|-|-|-|"
-git clone https://github.com/samsesh/SocialBox-Termux.git --quiet
-echo "Cloning grabcam:                  |-|-|-|-|"
-git clone https://github.com/noob-hackers/grabcam --quiet
-echo "Cloning igbrute:                  |-|-|-|"
-git clone https://github.com/dark-player/igbrute --quiet
-echo "Cloning ghost_brute:              |-|-|"
-git clone https://github.com/thegeniesghost/ghost_brute.git --quiet
-echo "Cloning facebash-termux:          |-|"
-git clone https://github.com/th3unkn0n/facebash-termux.git --quiet
+
+clone_if_exists() {
+  local url="$1"
+  local name="$2"
+  local bar="$3"
+
+  echo "Cloning $name: $bar"
+  
+  # Rimuove .git se presente per il controllo
+  check_url="${url%.git}"
+  
+  if curl --head --silent --fail "$check_url" > /dev/null; then
+    git clone "$url" --quiet
+  else
+    echo "❌ Repo $name non trovata o rimossa, salto."
+  fi
+}
+
+clone_if_exists "https://github.com/thehackingsage/hacktronian.git" "hacktronian"              "|-|-|-|-|-|-|-|-|-|-|-|-|"
+clone_if_exists "https://github.com/noob-hackers/ighack"             "ighack"                   "|-|-|-|-|-|-|-|-|-|-|-|"
+clone_if_exists "https://github.com/rajkumardusad/onex.git"         "onex"                     "|-|-|-|-|-|-|-|-|"
+clone_if_exists "https://github.com/jaykali/hackerpro.git"          "hackerpro"                "|-|-|-|-|-|-|-|"
+clone_if_exists "https://github.com/noob-hackers/infect"            "infect"                   "|-|-|-|-|-|-|"
+clone_if_exists "https://github.com/samsesh/SocialBox-Termux.git"   "SocialBox-Termux"         "|-|-|-|-|-|"
+clone_if_exists "https://github.com/noob-hackers/grabcam"           "grabcam"                  "|-|-|-|-|"
+clone_if_exists "https://github.com/dark-player/igbrute"            "igbrute"                  "|-|-|-|"
+clone_if_exists "https://github.com/thegeniesghost/ghost_brute.git" "ghost_brute"              "|-|-|"
+clone_if_exists "https://github.com/th3unkn0n/facebash-termux.git"  "facebash-termux"          "|-|"
+
 sleep 2
