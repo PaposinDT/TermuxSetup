a31b044e5ff44ec27688b8d76b24d786335ab021
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 10:36:11 +0200

    Update Information-Gathering.sh

--- Information-Gathering.sh
+++ Information-Gathering.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-#Author.......: Riccardo Papa
-mkdir ./Information-Gathering
+# Author.......: Riccardo Papa
+
+# Funzione per clonare solo se la repo esiste
+clone_if_exists() {
+  local url="$1"
+  local name="$2"
+  local bar="$3"
+
+  echo "Cloning $name: $bar"
+  if curl --head --silent --fail "${url%.git}" > /dev/null; then
+    git clone "$url" --quiet
+  else
+    echo "❌ Repo $name non trovata o rimossa, salto."
+  fi
+}
+
+mkdir -p ./Information-Gathering
 ./DDoS.sh
 cd ./Information-Gathering
+
 echo
-echo "INSTALLING INFORMATION GATHERING TOOLS"
+echo "📡 INSTALLING INFORMATION GATHERING TOOLS"
 sleep 2
 echo "Cloning 4 repositories"
 echo
 sleep 2
-echo "Cloning IP-Tracer:                |-|-|-|-|"
-git clone https://github.com/rajkumardusad/IP-Tracer.git --quiet
-echo "Cloning osi.ig:                   |-|-|-|"
-git clone https://github.com/th3unkn0n/osi.ig.git --quiet
-echo "Cloning TeleGram-Scraper:         |-|-|" 
-git clone https://github.com/th3unkn0n/TeleGram-Scraper.git --quiet
-echo "Cloning ipdrone:                  |-|"
-git clone https://github.com/noob-hackers/ipdrone --quiet
+
+clone_if_exists "https://github.com/rajkumardusad/IP-Tracer.git"           "IP-Tracer"           "|-|-|-|-|"
+clone_if_exists "https://github.com/th3unkn0n/osi.ig.git"                  "osi.ig"              "|-|-|-|"
+clone_if_exists "https://github.com/th3unkn0n/TeleGram-Scraper.git"       "TeleGram-Scraper"    "|-|-|"
+clone_if_exists "https://github.com/noob-hackers/ipdrone"                 "ipdrone"             "|-|"
+
 sleep 2
