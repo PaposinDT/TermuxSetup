fdf6b7f259bd56c458e8c926a4736991ec84a093
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 10:28:48 +0200

    Update Phishing.sh

--- Phishing.sh
+++ Phishing.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-#Author.......: Riccardo Papa
-mkdir ./Phishing
+# Author.......: Riccardo Papa
+
+mkdir -p ./Phishing
 ./Information-Gathering.sh
 cd ./Phishing
+
 echo
 echo "INSTALLING PHISHING TOOLS"
 sleep 2
 echo "Cloning 4 repositories"
 echo
 sleep 2
-echo "Cloning zphisher:                 |-|-|-|-|"
-git clone git://github.com/htr-tech/zphisher.git --quiet
-echo "Cloning nexphisher:               |-|-|-|"
-git clone git://github.com/htr-tech/nexphisher.git --quiet
-echo "Cloning maskphish:                |-|-|"
-git clone https://github.com/jaykali/maskphish --quiet
-echo "Cloning mrphish:                  |-|"
-git clone https://github.com/noob-hackers/mrphish --quiet
+
+clone_if_exists() {
+  local url="$1"
+  local name="$2"
+
+  echo "Cloning $name: $3"
+
+  # Verifica che la repo esista
+  if curl --head --silent --fail "${url%.git}" > /dev/null; then
+    git clone "$url" --quiet
+  else
+    echo "❌ Repo $name non trovata o rimossa, salto."
+  fi
+}
+
+clone_if_exists "git://github.com/htr-tech/zphisher.git" "zphisher" "               |-|-|-|-|"
+clone_if_exists "git://github.com/htr-tech/nexphisher.git" "nexphisher" "           |-|-|-|"
+clone_if_exists "https://github.com/jaykali/maskphish" "maskphish" "                |-|-|"
+clone_if_exists "https://github.com/noob-hackers/mrphish" "mrphish" "               |-|"
+
 sleep 2
