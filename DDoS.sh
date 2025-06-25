b70f520ca2acabb223d66088b52d2f6161596db3
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 10:36:44 +0200

    Update DDoS.sh

--- DDoS.sh
+++ DDoS.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
-#Author.......: Riccardo Papa
-mkdir ./DDoS
+# Author.......: Riccardo Papa
+
+clone_if_exists() {
+  local url="$1"
+  local name="$2"
+  local bar="$3"
+
+  echo "Cloning $name: $bar"
+  if curl --head --silent --fail "${url%.git}" > /dev/null; then
+    git clone "$url" --quiet
+  else
+    echo "❌ Repo $name non trovata o rimossa, salto."
+  fi
+}
+
+mkdir -p ./DDoS
 cd ./DDoS
+
 echo
-echo "INSTALLING DDOS TOOLS"
+echo "🚀 INSTALLING DDOS TOOLS"
 sleep 2
 echo "Cloning 4 repositories"
 echo
 sleep 2
-echo "Cloning Impulse:                  |-|-|-|-|"
-git clone https://github.com/LimerBoy/Impulse --quiet
-echo "Cloning XERXES:                   |-|-|-|"
-git clone https://github.com/XCHADXFAQ77X/XERXES.git --quiet
-echo "Cloning Vaim-sms:                 |-|-|" 
-git clone https://github.com/VaimpierOfficial/Vaim-sms --quiet
-echo "Cloning Ip-Attack:                |-|"
-git clone https://github.com/Bhai4You/Ip-Attack --quiet
+
+clone_if_exists "https://github.com/LimerBoy/Impulse.git"             "Impulse"        "|-|-|-|-|"
+clone_if_exists "https://github.com/XCHADXFAQ77X/XERXES.git"          "XERXES"         "|-|-|-|"
+clone_if_exists "https://github.com/VaimpierOfficial/Vaim-sms.git"    "Vaim-sms"       "|-|-|"
+clone_if_exists "https://github.com/Bhai4You/Ip-Attack.git"           "Ip-Attack"      "|-|"
+
 sleep 2
